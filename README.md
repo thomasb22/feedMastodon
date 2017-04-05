0c48915eb3d6c7d4e68fcffb0f89516710577a28
Thomas Blanschong <contact@thomasblanschong.fr>
2017-04-05 14:13:33 +0200

    Init

---
+++ README.md
@@ -0,0 +1,21 @@
+# feedMastodon
+
+Feed Mastodon with RSS Feeds
+
+## Requirements
+
+* Python 3
+* Mastodon API
+* feedparser
+
+## Installation and usage
+
+```
+git clone git@framagit.org:thomasb22/feedMastodon.git
+cd feedMastodon
+feedMastodon.py
+```
+
+## License
+
+[Gnu General Public License (GPL), Version 3 or later](https://www.gnu.org/licenses/gpl-3.0.html#SEC1)
---
+++ feedMastodon.py
@@ -0,0 +1,57 @@
+from mastodon import Mastodon
+import feedparser
+import os
+
+your_instance = 'https://mastodon.social'
+login = 'me@email.com'
+pwd = ''
+
+filename = 'feedMastodon-db.txt'
+feedurl = 'http://exemple.com/rss.xml'
+hashtags = '#feedMastodon #Mastodon'
+
+if not os.path.exists('feedMastodon-pytooter_clientcred.txt'):
+	Mastodon.create_app(
+		'feedMastodon',
+		to_file = 'feedMastodon-pytooter_clientcred.txt',
+		api_base_url = your_instance
+	)
+
+mastodon = Mastodon(
+	client_id = 'feedMastodon-pytooter_clientcred.txt',
+	api_base_url = your_instance
+)
+mastodon.log_in(
+	login,
+	pwd,
+	to_file = 'feedMastodon-pytooter_usercred.txt'
+)
+
+feed = feedparser.parse(feedurl)
+
+for item in reversed(feed.entries):
+	send = True
+	title = item.title
+	link = item.link
+	toot = title + ' ' + link
+
+	if hashtags:
+		toot += ' ' + hashtags
+
+	if os.path.exists(filename):
+		db = open(filename, "r+")
+		entries = db.readlines()
+	else:
+		db = open(filename, "a+")
+		entries = []
+
+	for entry in entries:
+		if link in entry:
+			send = False
+
+	if send:
+		mastodon.toot(toot)
+		db.write(link + '\n')
+		db.flush()
+
+db.close()
