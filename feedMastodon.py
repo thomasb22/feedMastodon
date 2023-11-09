32f1995a9e0d2eb4abe5a090f2c5e906ae0837d5
Thomas Blanschong <contact@thomasblanschong.fr>
2023-11-09 18:09:51 +0100

    Multi feed

--- feedMastodon.py
+++ feedMastodon.py
@@ -11,15 +11,15 @@ your_instance = 'https://mastodon.social'
 login = 'me@email.com'
 pwd = ''
 
-feedurl = 'http://exemple.com/rss.xml'
+feedsUrl = ['http://exemple.com/rss1.xml', 'http://exemple.com/rss2.xml']
 hashtags = '#feedMastodon #Mastodon'
 
 filename = 'feedMastodon-db.txt'
 tmpdir = 'tmp'
-show_summary = False
-show_picture = False
-maxtoots = 2
-maxchar = 500
+show_summary = [False, False]
+show_picture = [False, False]
+maxtoots = [2, 2]
+maxchar = [500, 500]
 
 if not os.path.exists('feedMastodon-pytooter_clientcred.txt'):
 	Mastodon.create_app(
@@ -33,82 +33,85 @@ mastodon = Mastodon(
 	api_base_url = your_instance
 )
 
-nbtoot = 0
-feed = feedparser.parse(feedurl)
+for idx, feedUrl in enumerate(feedsUrl):
+	nbtoot = 0
+	feed = feedparser.parse(feedUrl)
 
-for item in reversed(feed.entries):
-	send = True
-	soup = BeautifulSoup(item.title, 'lxml')
-	title = soup.text
-	soup = BeautifulSoup(item.summary, 'lxml')
-	summary = soup.text
-	link = item.link
-	toot = title + '\n\n' + link
+	for item in reversed(feed.entries):
+		send = True
+		soup = BeautifulSoup(item.title, 'lxml')
+		title = soup.text
+		soup = BeautifulSoup(item.summary, 'lxml')
+		summary = soup.text
+		link = item.link
+		toot = title + '\n\n' + link
 
-	if show_summary:
-		toot = title + '\n\n"' + summary + '"\n\n' + link
+		if show_summary[idx]:
+			toot = title + '\n\n"' + summary + '"\n\n' + link
 
-	if hashtags:
-		toot += '\n\n' + hashtags
-
-	if show_summary and len(toot) > maxchar and len(summary) > (len(toot) - maxchar) - 2:
 		if hashtags:
-			maxsum = len(summary) - (len(toot) - maxchar) - 2
-		else:
-			maxsum = len(summary) - (len(toot) - maxchar) - 1
-		toot = title + '\n\n"' + summary[:maxsum] + '…"\n\n' + link
-
-		if hashtags and len(toot) <= maxchar - (len(hashtags) + 1):
-			toot += '\n\n' + hashtags
-	elif len(toot) > maxchar and len(title) > (len(toot) - maxchar) - 1:
-		maxtitle = len(title) - (len(toot) - maxchar) - 1
-		toot = title[:maxtitle] + '… ' + link
-
-		if hashtags and len(toot) <= maxchar - (len(hashtags) + 1):
 			toot += '\n\n' + hashtags
 
-	if len(toot) > maxchar:
-		send = False
+		if show_summary[idx] and len(toot) > maxchar[idx] and len(summary) > (len(toot) - maxchar[idx]) - 2:
+			if hashtags:
+				maxsum = len(summary) - (len(toot) - maxchar[idx]) - 2
+			else:
+				maxsum = len(summary) - (len(toot) - maxchar[idx]) - 1
+			toot = title + '\n\n"' + summary[:maxsum] + '…"\n\n' + link
 
-	if os.path.exists(filename):
-		db = open(filename, "r+")
-		entries = db.readlines()
-	else:
-		db = open(filename, "a+")
-		entries = []
+			if hashtags and len(toot) <= maxchar[idx] - (len(hashtags) + 1):
+				toot += '\n\n' + hashtags
+		elif len(toot) > maxchar[idx] and len(title) > (len(toot) - maxchar[idx]) - 1:
+			maxtitle = len(title) - (len(toot) - maxchar[idx]) - 1
+			toot = title[:maxtitle] + '… ' + link
 
-	for entry in entries:
-		if link in entry:
+			if hashtags and len(toot) <= maxchar[idx] - (len(hashtags) + 1):
+				toot += '\n\n' + hashtags
+
+		if len(toot) > maxchar[idx]:
 			send = False
 
-	if send:
-		if nbtoot == 0:
-			mastodon.log_in(login, pwd)
-
-		if show_picture and item.enclosures:
-			if item.enclosures[0].type[:5] == 'image' and int(item.enclosures[0].length) <= 1000000:
-				tmpfilename = item.enclosures[0].href.split('/')[-1]
-				tmppath = tmpdir + '/' + tmpfilename
-				pictures_ids = []
-
-				if not os.path.exists(tmpdir):
-					os.mkdir(tmpdir)
-
-				wget.download(item.enclosures[0].href, tmppath)
-				pictures_ids.append( mastodon.media_post(tmppath) )
-				mastodon.status_post(toot, media_ids=pictures_ids)
-				os.remove(tmppath)
+		if os.path.exists(filename):
+			db = open(filename, "r+")
+			entries = db.readlines()
 		else:
-			mastodon.toot(toot)
+			db = open(filename, "a+")
+			entries = []
 
-		db.write(link + '\n')
-		db.flush()
+		for entry in entries:
+			if link in entry:
+				send = False
 
-		nbtoot = nbtoot + 1
-		if nbtoot >= maxtoots:
-			break
+		if send:
+			if nbtoot == 0:
+				mastodon.log_in(login, pwd)
 
-	db.close()
+			print( str(idx) + " " + str(show_picture[idx]) )
+
+			if show_picture[idx] and item.enclosures:
+				if item.enclosures[0].type[:5] == 'image' and int(item.enclosures[0].length) <= 1000000:
+					tmpfilename = item.enclosures[0].href.split('/')[-1]
+					tmppath = tmpdir + '/' + tmpfilename
+					pictures_ids = []
+
+					if not os.path.exists(tmpdir):
+						os.mkdir(tmpdir)
+
+					wget.download(item.enclosures[0].href, tmppath)
+					pictures_ids.append( mastodon.media_post(tmppath) )
+					mastodon.status_post(toot, media_ids=pictures_ids)
+					os.remove(tmppath)
+			else:
+				mastodon.toot(toot)
+
+			db.write(link + '\n')
+			db.flush()
+
+			nbtoot = nbtoot + 1
+			if nbtoot >= maxtoots[idx]:
+				break
+
+		db.close()
 
 if os.path.exists(tmpdir):
 	os.rmdir(tmpdir)
